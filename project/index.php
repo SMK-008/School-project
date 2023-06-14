0b2581824fec1d9a82a913a11b866113a18d5d27
Hezekiah Munene <hezemunene01@gmail.com>
2023-06-15 00:27:50 +0300

    created home page after successful login and implemente logout sessions

---
+++ project/index.php
@@ -0,0 +1,25 @@
+<?php
+    require 'function.php';
+    if(isset($_SESSION["id"])){
+        $id = $_SESSION["id"];
+        $user = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM patients WHERE ssn = $id"));
+    }else{
+        header("Location: login.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient HomePage</title>
+</head>
+<body>
+    <h1>Welcome
+        <?php echo $user['patient_name']; ?>
+    </h1>
+
+    <a href = "logout.php">Logout</a>
+</body>
+</html>
