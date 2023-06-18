eeebd6479f849ee75f08a58517745c42aaacbb35
Hezekiah Munene <hezemunene01@gmail.com>
2023-06-18 21:16:30 +0300

    added css to make name appear on the right

--- project/index.php
+++ project/index.php
@@ -14,12 +14,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient HomePage</title>
+
+    <link rel="stylesheet" href="patient_screen.css">
 </head>
 <body>
-    <h1>Welcome
+    <h1>
         <?php echo $user['patient_name']; ?>
     </h1>
 
-    <a href = "logout.php">Logout</a>
+    <a href = "logout.php" class= "logout-link">Logout</a>
+
 </body>
 </html>
