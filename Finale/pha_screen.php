0da64eb74382586619ee385944b7fd1355f101c8
H-Munene <106272021+H-Munene@users.noreply.github.com>
2023-07-21 12:11:06 +0300

    retained logo changed display layout

--- Finale/pha_screen.php
+++ Finale/pha_screen.php
@@ -1,31 +1,7 @@
-<?php
-    require 'function_ph.php';
-    if(isset($_SESSION["id"])){
-        $id = $_SESSION["id"];
-        $user = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM pharmacists WHERE p_pwd = $id"));
-    }else{
-        header("Location: login_ph.php");
-    }
-?>
-<!DOCTYPE html>
-<html>
-
 <head>
   <title>Pharmacist Homepage</title>
   <link rel="stylesheet" type="text/css" href="homepageeg.css">
-
-  <script>
-    function displayForm() {
-      var form = document.getElementById("myForm");
-      if (form.style.display === "none") {
-        form.style.display = "block";
-      } else {
-        form.style.display = "none";
-      }
-    }
-  </script>
-
-  <script>
+ <script>
       function toggleTable() {
         var table = document.getElementById("myTable");
         if (table.style.display === "none") {
@@ -46,34 +22,25 @@
         }
       }
     </script>
-
-  <script>
-    function clearTable() {
-      var table = document.getElementById("myTable");
-      table.innerHTML = "";
-    }
-  </script>
-
 </head>
 <body>
   <header>
-    <div class="logo">Your Logo</div>
+  <div class="logo"><img src='images/logo.png'></div>
     <div class="navbar">
       <nav>
         <ul>
-          <li><a href="eg.html">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
+
+          <li><a href="#">Account</a></li>
           <li><a href="#"><?php echo $user['p_name']; ?></a></li>
-    
-          <li><a href="logout_ph.php">Logout</a></li> <!-- Visible when logged in -->
-          
+          <li><a href="logout_a.php"><img src="images/logout.png" alt="logout banner"></a></li> <!-- Visible when logged in -->
+
         </ul>
       </nav>
+
     </div>
   </header>
- 
-  
+  <section>
+
   <button onclick="toggleTable()">View Prescriptions</button>
   
     <div id = "myTable" class ="bs-example" style = "display : none">
@@ -129,7 +96,7 @@
     <br>
 
     <button onclick="toggleTable2()">Administered Prescriptions</button>
-    <div id = "myTable2" class ="bs-example2" style = "display : none">
+    <div id = "myTable2" class ="bs-example"  style = "display : none">
     <div class="container">
     <div class="row">
     <div class="col-md-12">
@@ -137,45 +104,18 @@
     <h2 id = "myTable" class="pull-left">Administered Drugs</h2>
     </div>
     <?php
-    include_once 'drugsconnect.php';
+    include_once 'connect.php';
     $result = mysqli_query($conn,"SELECT * FROM prescriptions");
     ?>
     <?php
-    if (mysqli_num_rows($result) > 0) {
-    ?>
-    <table class='table table-bordered table-striped'>
-    <tr>
-    <td>Patient Name</td>
-    <td>Drug Name</td>
-    <td>Drug Amount</td>
-    <td>Frequency</td>
-    </tr>
-    <?php
-    $i=0;
-    while($row = mysqli_fetch_array($result)) {
-    ?>
-    <tr>
-    <td><?php echo $row['patientName']; ?></td>
-    <td><?php echo $row["drugName"]; ?></td>
-    <td><?php echo $row["drugAmount"]; ?></td>
-    <td><?php echo $row["frequency"]; ?></td>
-    </tr>
-    <?php
-    $i++;
-    }
-    ?>
-    </table>
-    <?php
-    }
-    else{
-    echo "No result found";
-    }
-    ?>
-    </div>
+	@@ -170,10 +175,7 @@ function clearTable() {
     </div>        
     </div>
     </div>
+  </section>
 
-
+  <footer>
+    <p>&copy; 2023 Your Company. All rights reserved.</p>
+  </footer>
 </body>
 </html>
